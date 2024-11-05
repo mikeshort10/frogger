9a6d6c8be58b501e42735929b3004325b5b30372
Mike Short <mikeshort2014@gmail.com>
2024-11-05 13:40:44 -0500

    feat: working score, no losing

--- setup.js
+++ setup.js
@@ -7,6 +7,8 @@ const createCanvas = (id) => {
   return [canvas, ctx]
 }
 
+
+
 const [canvas1, ctx1] = createCanvas("canvas1")
 const [canvas2, ctx2] = createCanvas("canvas2")
 const [canvas3, ctx3] = createCanvas("canvas3")
@@ -22,8 +24,43 @@ let collisionsCount = 0
 let frame = 0
 let gameSpeed = 1
 
-const particlesArray = []
-const maxParticles = 300
-const ripplesArray = []
-const carsArray = []
-const logsArray = []
+var particlesArray = []
+var maxParticles = 300
+var ripplesArray = []
+var carsArray = []
+var logsArray = []
+
+const background_lvl2 = new Image()
+background_lvl2.src = 'background_lvl2.png'
+
+const grass = new Image()
+background_lvl2.src = 'grass.png'
+
+const collision = new Image()
+background_lvl2.src = 'collisions.png'
+
+class KeyboardHandler {
+  constructor() {
+
+  }
+
+  static isLeftKey() {
+    return keys[37] === true
+  }
+
+  static isRightKey() {
+    return keys[39] === true
+  }
+
+  static isUpKey() {
+    return keys[38] === true
+  }
+
+  static isDownKey() {
+    return keys[40] === true
+  }
+
+  static isKeyboardKey() {
+    return KeyboardHandler.isRightKey() || KeyboardHandler.isLeftKey() || KeyboardHandler.isUpKey() || KeyboardHandler.isDownKey()
+  }
+}
